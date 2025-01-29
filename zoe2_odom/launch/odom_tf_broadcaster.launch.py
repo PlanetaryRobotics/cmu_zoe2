4771979f81ffe350c417285511c64b8dc7b4d3f2
eholandcmu <eholand@andrew.cmu.edu>
2025-01-29 10:58:55 -0500

    Update RViz configuration and fix launch file command for odom_tf_broadcaster

--- zoe2_odom/launch/odom_tf_broadcaster.launch.py
+++ zoe2_odom/launch/odom_tf_broadcaster.launch.py
@@ -6,7 +6,9 @@ from launch.substitutions import LaunchConfiguration
 def generate_launch_description():
     return LaunchDescription([
         ExecuteProcess(
-            cmd=['ros2', 'run', 'zoe_odom', 'odom_tf_broadcaster', '--ros-args', '-p', 'use_sim_time:=true'],
+            cmd=['ros2', 'run', 'zoe2_odom', 'odom_tf_broadcaster', '--ros-args', '-p', 'use_sim_time:=true'],
             output='screen'
         ),
     ])
+
+
