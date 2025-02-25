2e62b03d8329e364e5a9f19c322ed93ece3f168c
eholandcmu <eholand@andrew.cmu.edu>
2025-02-25 13:12:20 -0500

    Clean up zoe_controller by removing console printouts and commented out blocks

--- zoe_controller/src/zoe_controller.cpp
+++ zoe_controller/src/zoe_controller.cpp
@@ -64,13 +64,7 @@ controller_interface::CallbackReturn ZoeController::on_init() {
     }
 
     RCLCPP_INFO(log(), "starting the Zoe controller@@@@@@@@@@@@@@@@@");
-    // currDriveArc = std::make_shared<zoe2_interfaces::msg::DriveArc>();
-    // currDriveArc->radius = 0.0;
-    // currDriveArc->speed = 0;
-    // currDriveArc->sender = "";
-    // currDriveArc->time = 0;
-    // currDriveTimer = nullptr;
-    // TODO: Change following to load in params from xacro
+    // todo: make this use rosparams instead of being hardcoded
     controller = std::make_shared<DrivingController>(1.64, 0.325, 1.91, 5.0);
 
     return controller_interface::CallbackReturn::SUCCESS;
@@ -103,28 +97,9 @@ InterfaceConfiguration ZoeController::state_interface_configuration() const {
 
 rclcpp::Logger serv_logger() { return rclcpp::get_logger("drive-command"); }
 
-// void ZoeDriveCommandService(
-//     ZoeController *impl,
-//     const zoe2_interfaces::srv::DriveCommand::Request::SharedPtr request,
-//     zoe2_interfaces::srv::DriveCommand::Response::SharedPtr response) {
-
-//     RCLCPP_INFO(impl->log(), "got a command ");
-
-//     impl->currDriveArcMutex.lock();
-//     impl->currDriveArc->radius = request->drive_arc.radius;
-//     impl->currDriveArc->speed = request->drive_arc.speed;
-//     impl->currDriveArc->sender = request->drive_arc.sender;
-//     impl->currDriveArc->time = request->drive_arc.time;
-//     impl->currDriveArcMutex.unlock();
-
-//     response->success = true;
-// }
-
 controller_interface::return_type
 ZoeController::update(const rclcpp::Time &time,
                       const rclcpp::Duration &period) {
-
-    RCLCPP_INFO(log(), "Got an update ");
 
     std::shared_ptr<Twist> last_command_msg;
     received_velocity_msg_ptr_.get(last_command_msg);
@@ -160,15 +135,6 @@ ZoeController::update(const rclcpp::Time &time,
     auto speed = linear_command;
     auto radius = (std::abs(angular_command) < 1e-6) ? std::numeric_limits<double>::max() : (linear_command / angular_command);
 
-    RCLCPP_INFO(log(), "Radius: %f, Speed: %f", radius, speed);
-
-
-    // currDriveArcMutex.lock();
-    // auto speed = currDriveArc->speed;
-    // auto radius = currDriveArc->radius;
-    // auto sender = currDriveArc->sender;
-    // auto time_left = currDriveArc->time;
-
     controller->setTarget(radius, speed);
 
     controller->setVfl(frontLeft->feedback_vel.get().get_value() * controller->getWheelRadius());
@@ -180,51 +146,17 @@ ZoeController::update(const rclcpp::Time &time,
     controller->setThetab(backYaw->yaw.get().get_value());
     
     controller->computeWheelSpeed();
-    // currDriveArcMutex.unlock();
-
 
     frontLeft->cmd_velocity.get().set_value(controller->getcVfl()/controller->getWheelRadius());
     frontRight->cmd_velocity.get().set_value(controller->getcVfr()/controller->getWheelRadius());
     backLeft->cmd_velocity.get().set_value(controller->getcVbl()/controller->getWheelRadius());
     backRight->cmd_velocity.get().set_value(controller->getcVbr()/controller->getWheelRadius());
 
-    // currDriveArcMutex.lock();
-    // currDriveArc->time -= period.seconds();
-
-    // if (currDriveArc->time <= 0) {
-    //     currDriveArc->radius = 0;
-    //     currDriveArc->speed = 0;
-    //     currDriveArc->sender = "";
-    //     currDriveArc->time = 0;
-    // }
-    // currDriveArcMutex.unlock();
-
     return controller_interface::return_type::OK;
 }
-// void OnDriveArcMsg(ZoeController *impl,
-//                    zoe2_interfaces::msg::DriveArc::ConstSharedPtr driveArc) {
-//     RCLCPP_INFO(serv_logger(), "drive arc message %s",
-//                 driveArc->sender.c_str());
-// }
 
 controller_interface::CallbackReturn
 ZoeController::on_configure(const rclcpp_lifecycle::State &) {
-
-    // TODO: setup odom and transform publishers and subscribers
-
-    // drive_command_service =
-    //     get_node()->create_service<zoe2_interfaces::srv::DriveCommand>(
-    //         "/zoe_drive",
-    //         std::bind(&ZoeDriveCommandService, this, std::placeholders::_1,
-    //                   std::placeholders::_2));
-    // mCommandVelSubscriber =
-    //     get_node()->create_subscription<zoe2_interfaces::msg::DriveArc>(
-    //         "/drive_cmd_curr", rclcpp::QoS(1),
-    //         [this](zoe2_interfaces::msg::DriveArc::ConstSharedPtr arc) {
-    //             OnDriveArcMsg(this, arc);
-    //         });
-
-    // RCLCPP_INFO(log(), "started /zoe_drive and /drive_cmd_curr");
 
     const Twist empty_twist;
     received_velocity_msg_ptr_.set(std::make_shared<Twist>(empty_twist));
