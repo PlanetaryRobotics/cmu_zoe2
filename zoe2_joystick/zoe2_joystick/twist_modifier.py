ebe354215dc9585bc297fbce45214c1986d2233a
zoe-rover <zoe2.rover@gmail.com>
2025-08-18 16:26:47 -0400

    Updated the joystick controller publish angular velocity and not turn radius

--- zoe2_joystick/zoe2_joystick/twist_modifier.py
+++ zoe2_joystick/zoe2_joystick/twist_modifier.py
@@ -17,34 +17,37 @@ class TwistModifier(Node):
         # Create a subscriber to the topic published by the joystick
         self.subscription = self.create_subscription(
             Twist,
-            'cmd_vel_raw',
+            '/cmd_vel_raw',
             self.listener_callback,
             10)
         
         # Create a publisher to the modified message for the controller
-        self.publisher_ = self.create_publisher(Twist, 'cmd_vel_unstamped', 10)
+        self.publisher_ = self.create_publisher(Twist, '/cmd_vel_unstamped', 10)
         
     def listener_callback(self, msg):
         """
         Callback function for the subscriber. This function is called every time
         a new message is received on the subscribed topic.
         """
-        self.get_logger().info(f'Received Twist message: linear.x={msg.linear.x}, angular.z={msg.angular.z}')
+        #self.get_logger().info(f'Received Twist message: linear.x={msg.linear.x}, angular.z={msg.angular.z}')
 
         # Create a new Twist message for publishing
         modified_twist = Twist()
         
         # Perform the mathematical operation: scale the linear and angular components
         modified_twist.linear.x = msg.linear.x
-        if math.abs(msg.angular.z) < 0.1:
-            modified_twist.angular.z = 1000.0
+        turn_radius = 0.0
+        if math.fabs(msg.angular.z) < 0.1:
+            turn_radius = 1000.0
         else:
-            modified_twist.angular.z = -4.0 * math.tan(msg.angular.z - math.pi/2.0)
+            turn_radius = -2.0 * math.tan(msg.angular.z - math.pi/2.0)
         
+        modified_twist.angular.z = msg.linear.x / turn_radius if turn_radius != 0 else 0.0
+
         # Publish the modified message
         self.publisher_.publish(modified_twist)
         
-        self.get_logger().info(f'Published modified Twist: linear.x={modified_twist.linear.x}, angular.z={modified_twist.angular.z}')
+        #self.get_logger().info(f'Published modified Twist: linear.x={modified_twist.linear.x}, angular.z={modified_twist.angular.z}')
 
 def main(args=None):
     # Initialize the rclpy library
