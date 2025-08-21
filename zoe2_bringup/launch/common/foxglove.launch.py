b0a4628dedce7cbac77e39cb4f60df8c062b5c5d
eholandcmu <eholand@andrew.cmu.edu>
2025-08-20 21:27:16 -0400

    Add foxglove_bridge launch file and update package dependencies

--- zoe2_bringup/launch/common/foxglove.launch.py
+++ zoe2_bringup/launch/common/foxglove.launch.py
@@ -6,15 +6,17 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
+from launch_ros.actions import Node
+
 def generate_launch_description():
 
     # launch the vectornav sensor
-    vectornav_node = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory("vectornav"),'launch','vectornav.launch.py'
-        )])
+    foxglove_bridge = Node(
+        package="foxglove_bridge",
+        executable="foxglove_bridge",
+        output="screen"
     )
 
     return LaunchDescription([
-        vectornav_node
+        foxglove_bridge
     ])
