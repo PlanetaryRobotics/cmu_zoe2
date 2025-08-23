51e81821a9508fcd076b2949e43d826b28739d28
Zoë Computer <zoe2.rover@gmail.com>
2025-08-23 11:40:36 -0400

    Refactor cmd_vel_publisher to ramp up linear velocity over time

--- zoe2_utils/cmd_vel_publisher/cmd_vel_publisher/cmd_vel_pub.py
+++ zoe2_utils/cmd_vel_publisher/cmd_vel_publisher/cmd_vel_pub.py
@@ -7,21 +7,27 @@ class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('cmd_vel_publisher')
-        self.publisher_ = self.create_publisher(Twist, '/cmd_vel_test', 10)
+        self.publisher_ = self.create_publisher(Twist, '/cmd_vel_unstamped', 10)
         timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        self.current_vx = 0.0
+        self.vx_increment = 0.001  # m/s per timer tick
+        self.max_vx = 1.0  # m/s
 
     def timer_callback(self):
-        # Create a new Twist message for publishing
-        vx = 0.25  # m/s
+        # Ramp up vx linearly until max_vx
         turn_radius = 1.5  # m
         twist_msg = Twist()
-        twist_msg.linear.x = vx
-        twist_msg.angular.z = vx / turn_radius
+        twist_msg.linear.x = self.current_vx
+        twist_msg.angular.z = self.current_vx / turn_radius
 
         self.publisher_.publish(twist_msg)
-        self.get_logger().info('Publishing: "%s"' % twist_msg)
+        self.get_logger().info(f'Publishing: vx={self.current_vx:.2f}, angular.z={twist_msg.angular.z:.2f}')
+
+        if self.current_vx < self.max_vx:
+            self.current_vx += self.vx_increment
+            if self.current_vx > self.max_vx:
+                self.current_vx = self.max_vx
 
 
 def main(args=None):
